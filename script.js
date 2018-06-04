97c2e12fa2e7f5f90715899b116d4624800180d4
Ahmed Mohamed <32383299+AhmedMohamedEid@users.noreply.github.com>
2018-06-04 11:54:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 (function () {
 
-var names = ["Ahmed", "Mohamed", "eid", "Ali", "Alaa", "Sara"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
 for (var i = 0; i < names.length; i++) {
